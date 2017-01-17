c63dc9eabb03ff8e0d3fdc1aa7c7fc59554d528e
arnuga3 <arniszelcs@gmail.com>
2017-01-17 01:22:49 +0000

    changed directory

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,1 @@
-$(window).on('load', function() {
-
-	// this will fire after the entire page is loaded, including images
-	//param is the navbar height of the page
-	$(".sliderImages").slider_z(0);
-	$(".sliderControl img:first").click();
-
-	// three-dot menu btn toggle animation
-	$('#menuBtn').on('click', function() {
-		$('#menuContainer').animate({width: "toggle", height: "toggle"}, 'fast');
-	});
-});
+$(window).on("load",function(){$(".sliderImages").slider_z(0),$(".sliderControl img:first").click(),$("#menuBtn").on("click",function(){$("#menuContainer").animate({width:"toggle",height:"toggle"},"fast")})});
